a3df8c6a0cbfeaa0a21f4b74b2bde045012ae5fd
Benoit POLASZEK <=>
2025-10-16 13:42:32 +0200

    Docs: Add README and LICENSE files Refactor: Simplify ClassMetadata and PropertyMetadata handling Test: Update CriteriaBuilder tests for reflection-based metadata setup

--- src/Config/AsDocument.php
+++ src/Config/AsDocument.php
@@ -5,48 +5,21 @@ declare(strict_types=1);
 namespace Honey\ODM\Meilisearch\Config;
 
 use Attribute;
-use Honey\ODM\Core\Config\ClassMetadataInterface;
-use Honey\ODM\Core\Config\PropertyMetadataInterface;
-use ReflectionClass;
+use Honey\ODM\Core\Config\ClassMetadata;
 use RuntimeException;
-
-use function array_find;
 
 /**
  * @template O of object
  * @template P of AsAttribute
  *
- * @implements ClassMetadataInterface<O, P>
+ * @extends ClassMetadata<O, P>
  */
 #[Attribute(Attribute::TARGET_CLASS)]
-final class AsDocument implements ClassMetadataInterface
+final class AsDocument extends ClassMetadata
 {
-    /**
-     * @var class-string<O>
-     */
-    public string $className;
-
-    /**
-     * @var ReflectionClass<O>
-     */
-    public ReflectionClass $reflection;
-
-    /**
-     * @var array<string, P>
-     */
-    public array $propertiesMetadata = [];
-
     public function __construct(
-        public string $index,
+        public readonly string $index,
     ) {
-    }
-
-    public function getIdPropertyMetadata(): PropertyMetadataInterface
-    {
-        return array_find(
-            $this->propertiesMetadata,
-            fn (PropertyMetadataInterface $metadata) => $metadata->primary,
-        ) ?? throw new RuntimeException('No primary property found in class metadata');
     }
 
     public function getAttributeName(string $propertyName): string
