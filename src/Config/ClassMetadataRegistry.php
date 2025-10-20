319022229188814b7208c2a6ff8fd3b9f5e14616
Beno!t POLASZEK <bpolaszek@gmail.com>
2025-10-20 06:37:47 +0200

    feat: add filterable/sortable attributes and handle schema updates

--- src/Config/ClassMetadataRegistry.php
+++ src/Config/ClassMetadataRegistry.php
@@ -6,11 +6,14 @@ namespace Honey\ODM\Meilisearch\Config;
 
 use Honey\ODM\Core\Config\ClassMetadataRegistryInterface;
 use Honey\ODM\Core\Config\ClassMetadataRegistryTrait;
+use IteratorAggregate;
+use Traversable;
 
 /**
  * @implements ClassMetadataRegistryInterface<AsDocument<object, AsAttribute>, AsAttribute>
+ * @implements IteratorAggregate<class-string, AsDocument<object, AsAttribute>>
  */
-final class ClassMetadataRegistry implements ClassMetadataRegistryInterface
+final class ClassMetadataRegistry implements ClassMetadataRegistryInterface, IteratorAggregate
 {
     /**
      * @use ClassMetadataRegistryTrait<AsDocument, AsAttribute>
@@ -39,5 +42,10 @@ final class ClassMetadataRegistry implements ClassMetadataRegistryInterface
         $propertyName = $idPropertyMetadata->name ?? $idPropertyMetadata->reflection->name;
 
         return $this->propertyAccessor->getValue((object) $document, $propertyName);
+    }
+
+    public function getIterator(): Traversable
+    {
+        yield from $this->storage; // @phpstan-ignore generator.valueType
     }
 }
